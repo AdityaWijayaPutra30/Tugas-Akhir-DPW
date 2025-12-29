c86e5bd83d353bb4af340c185f1c82b4f32b8c7f
Codhie <swarigadingfadhil@gmail.com>
2025-12-29 14:13:12 +0700

    a

--- resources/views/perpus/userdashboard.blade.php
+++ resources/views/perpus/userdashboard.blade.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>User Dashboard</title>
 </head>
+
 <body>
- <h3>INI USER DASHBOARD</h3>
+    <h3>INI USER DASHBOARD</h3>
 
- <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi doloribus quos itaque saepe animi nesciunt, expedita repudiandae? Assumenda rerum magni excepturi tempora. Officiis porro reiciendis molestiae impedit commodi quae suscipit.</p>
-
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi doloribus quos itaque saepe animi nesciunt, expedita repudiandae? Assumenda rerum magni excepturi tempora. Officiis porro reiciendis molestiae impedit commodi quae suscipit.</p>
 
 </body>
+
 </html>
